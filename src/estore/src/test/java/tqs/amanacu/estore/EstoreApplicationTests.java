41266f4d7725154556b2b5ba3bfd95d336cd3527
Bruno Rocha Moura <brunorochamoura@proton.me>
2023-05-17 13:02:00 +0100

    Spring boot project created

---
+++ src/estore/src/test/java/tqs/amanacu/estore/EstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs.amanacu.estore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

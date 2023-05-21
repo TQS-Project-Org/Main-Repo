9cc6cb0a9bd7f05f3f45288d6eb3ba725b4fd76b
jteles277 <59253209+jteles277@users.noreply.github.com>
2023-05-21 11:02:41 +0100

    Tests are now seen

--- src/estore/src/test/java/tqs/amanacu/estore/AuthorizationControllerSteps.java
+++ src/estore/src/test/java/tqs/amanacu/estore/AuthorizationControllerSteps.java
@@ -3,43 +3,38 @@ package tqs.amanacu.estore;
 import tqs.amanacu.estore.repositories.UserRepository;
 import tqs.amanacu.estore.security.JwtUtils;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-@SpringBootTest
-@ContextConfiguration
+@SpringBootTest 
+@AutoConfigureMockMvc
 public class AuthorizationControllerSteps {
-  
-  private MockMvc mockMvc;
+   
   private ResultActions resultActions;
   private MvcResult mvcResult;
   private String requestBody;
 
   @Autowired
-  private WebApplicationContext webApplicationContext;
+  private MockMvc mockMvc;
 
   @MockBean
   private UserRepository userRepository;
@@ -49,14 +44,7 @@ public class AuthorizationControllerSteps {
 
   @MockBean
   private PasswordEncoder encoder;
-
-  @MockBean
-  private JwtUtils jwtUtils;
-
-  @Before
-  public void setup() {
-    mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-  }
+ 
 
   @Given("^a login request with username \"(.*)\" and password \"(.*)\"$")
   public void createLoginRequest(String username, String password) {
--- src/estore/src/test/java/tqs/amanacu/estore/CucTest.java
+++ src/estore/src/test/java/tqs/amanacu/estore/CucTest.java
@@ -1,5 +1,6 @@
 package tqs.amanacu.estore;
 
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
@@ -10,7 +11,8 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("tqs/amanacu/estore")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "tqs.amanacu.estore")
-public class Runner {
+public class CucTest {
     // This is just a runner class, no implementation needed here
 }
